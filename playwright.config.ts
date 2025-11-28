918615aeb1d26954adbf535d4e628e3d5f00a315
Andreea Eftene <hi@pixelle.ro>
2025-11-28 22:39:27 +0200

    Change Playwright base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
   reporter: process.env.CI ? 'github' : 'list',
 
   use: {
-    baseURL: 'http://localhost:5174',
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
 
   webServer: {
     command: 'pnpm dev',
-    url: 'http://localhost:5174',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 30_000,
   },
